0a1548e0f7deb503ba68e73ed62ab7d03246aadd
Lwl <weilong.liu-csu@outlook.com>
2021-03-23 08:24:43 +0800

    first commit

---
+++ src/Camera.h
@@ -0,0 +1,16 @@
+/*
+ * @Author: Liu Weilong
+ * @Date: 2021-03-22 19:23:05
+ * @LastEditors: Liu Weilong
+ * @LastEditTime: 2021-03-22 19:24:20
+ * @Description: 
+ */
+
+#include "Eigen/Eigen"
+
+class Camera
+{
+  public:
+  double fx,fy,cx,cy;
+    
+};
